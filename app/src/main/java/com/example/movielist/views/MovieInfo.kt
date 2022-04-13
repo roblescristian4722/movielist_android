adcc50dea2733a13c19656c49db7ced2058185a8
Cristian Robles <cristian.robles@accedo.tv>
2022-04-13 13:11:20 -0500

    sample mp4 video gets displayed on fullscreen mode when button is pressed

--- app/src/main/java/com/example/movielist/views/MovieInfo.kt
+++ app/src/main/java/com/example/movielist/views/MovieInfo.kt
@@ -1,50 +1,46 @@
 package com.example.movielist.views
 
-import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.example.movielist.R
 import com.example.movielist.databinding.FragmentMovieInfoBinding
 import com.example.movielist.viewmodel.MovieViewModel
 import com.google.android.exoplayer2.ExoPlayer
-import com.google.android.exoplayer2.ui.StyledPlayerView
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.google.android.exoplayer2.MediaItem
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class MovieInfo : Fragment() {
-    private val movieViewModel: MovieViewModel by sharedViewModel()
     private lateinit var binding: FragmentMovieInfoBinding
-    private var player: ExoPlayer? = null
-
-    private fun instantiatePlayer() {
-        if (player == null) {
-            player = ExoPlayer.Builder(requireContext()).build()
-                .also {
-
-                }
-        }
-    }
+    private val movieViewModel: MovieViewModel by sharedViewModel()
+    private val player: ExoPlayer by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmentTransaction = childFragmentManager.beginTransaction()
-        fragmentTransaction.add(R.id.fl_player_container, VideoPlayer(), "video_player")
-        fragmentTransaction.commit()
-
+        // Creates a MediaItem out of a sample mp4 video to stream into the player
+        val mediaItem = MediaItem.fromUri(Uri.parse(getString(R.string.sample_mp4_video)))
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_movie_info, container, false)
         binding.movieInfo = movieViewModel
+
+        binding.btnPlayTrailer.setOnClickListener {
+            // When button is pressed then we play the video and navigate to VideoPlayer fragment so
+            // that fullscreen gets enabled
+            player.setMediaItem(mediaItem)
+            player.prepare()
+            player.play()
+            findNavController().navigate(R.id.action_movieInfo_to_videoPlayer)
+        }
 
         movieViewModel.selectedMovieLiveData.observe(viewLifecycleOwner, Observer {
             movieViewModel.configurationLiveData.value?.let { configuration ->
--- app/src/main/java/com/example/movielist/views/VideoPlayer.kt
+++ app/src/main/java/com/example/movielist/views/VideoPlayer.kt
@@ -1,14 +1,14 @@
 package com.example.movielist.views
 
+import android.os.Build
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.example.movielist.R
 import com.example.movielist.databinding.FragmentVideoPlayerBinding
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import org.koin.android.ext.android.inject
 
 class VideoPlayer : Fragment() {
@@ -22,7 +22,30 @@ class VideoPlayer : Fragment() {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_video_player, container, false)
         binding.spvPlayer.player = player
+        binding.spvPlayer.resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FILL
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Enables fullscreen mode both for API < 30 and API >= 30
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requireActivity().window.insetsController?.hide(WindowInsets.Type.statusBars())
+            requireActivity().window.insetsController?.hide(WindowInsets.Type.navigationBars())
+        } else {
+            requireActivity().window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        // Disables fullscreen mode both for API < 30 and API >= 30 when back button is pressed
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            requireActivity().window.insetsController?.show(WindowInsets.Type.statusBars())
+            requireActivity().window.insetsController?.show(WindowInsets.Type.navigationBars())
+        } else {
+            requireActivity().window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
     }
 }
